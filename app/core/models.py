f6235c6fc54b177fb98bd63d5e06b91b0230d38e
Randy Bravo <rbroh99@gmail.com>
2024-04-06 08:19:36 -0400

    Implemented user model and its tests

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,48 @@
-from django.db import models # noqa
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager,
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for the users."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user."""
+        if not email:
+            raise ValueError('User must have an email!.')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create superuser with given details."""
+        user = self.model(email=email, password=password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        """Returns the user string representation."""
+        return self.email
